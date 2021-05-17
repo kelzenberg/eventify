71bab9c7b3b764840082fb88e1e1d201fc37b9db
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-05-17 21:38:36 +0200

    format api project

--- api/README.md
+++ api/README.md
@@ -5,11 +5,13 @@ The eventify REST API, build with Spring.
 ## Quickstart
 
 For the initial start, set the local environment variables:
+
 ```sh
 cp .env.example local.env
 ```
-Afterwards use Maven to build and run the project.
-A run configuration for _IntelliJ IDEA_ is located in the `.run` folder.
+
+Afterwards use Maven to build and run the project. A run configuration for _IntelliJ IDEA_ is located in the `.run`
+folder.
 
 ## Entities Schema
 
--- api/src/main/java/com/eventify/api/entities/PersistenceConfig.java
+++ api/src/main/java/com/eventify/api/entities/PersistenceConfig.java
@@ -1,11 +1,15 @@
 package com.eventify.api.entities;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Optional;
 
 
 @Configuration
@@ -18,4 +22,13 @@ public class PersistenceConfig {
     public AuditorAware<String> auditorProvider() {
         return new AuditorAwareProvider();
     }
+
+    private static class AuditorAwareProvider implements AuditorAware<String> {
+        @Override
+        @NonNull
+        public Optional<String> getCurrentAuditor() {
+            return Optional.of(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+    }
+
 }
