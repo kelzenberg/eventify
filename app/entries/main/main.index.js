d3e7757bb229df2ac74e846b8b6a60d7b74804f3
Jannis Rieger <omniskopus@gmail.com>
2021-05-26 22:45:20 +0200

    redirect to landing page when not authenticated

--- app/entries/main/main.index.js
+++ app/entries/main/main.index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoadingPage from '../../pages/Loading';
+import * as stateKeeper from '../../common/stateKeeper';
 import "../../common.scss";
 
 const EventsOverviewPage = React.lazy(() => import('../../pages/EventsOverview/EventsOverview'));
 const EventPage = React.lazy(() => import('../../pages/Event/Event'));
+
+if(!stateKeeper.isAuthenticated()) {
+    window.location = "/";
+}
 
 ReactDOM.render(<MainPage/>, document.getElementById('root'));
 
