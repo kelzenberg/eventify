2dbc9f91a90744738c640bc37fd928c5144c65eb
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-06-04 00:47:26 +0200

    create Admin endpoints for roles

--- api/src/main/java/com/eventify/api/entities/usereventrole/controllers/UserEventRoleController.java
+++ api/src/main/java/com/eventify/api/entities/usereventrole/controllers/UserEventRoleController.java
@@ -5,10 +5,7 @@ import com.eventify.api.constants.EventRole;
 import com.eventify.api.entities.usereventrole.data.UserEventRole;
 import com.eventify.api.entities.usereventrole.services.UserEventRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -23,6 +20,15 @@ public class UserEventRoleController {
     @GetMapping(AdminPaths.EVENT_ROLES)
     List<UserEventRole> getAll() {
         return service.getAll();
+    }
+
+    @GetMapping(AdminPaths.EVENT_ROLES + "/user/{userId}")
+    List<UserEventRole> getAllByUserId(@PathVariable UUID userId) {
+        return service.getByUserId(userId);
+    }
+    @GetMapping(AdminPaths.EVENT_ROLES + "/event/{eventId}")
+    List<UserEventRole> getAllByEventId(@PathVariable UUID eventId) {
+        return service.getByEventId(eventId);
     }
 
     @PostMapping(AdminPaths.EVENT_ROLES)
--- api/src/main/java/com/eventify/api/entities/usereventrole/controllers/UserEventRoleCreateRequest.java
+++ api/src/main/java/com/eventify/api/entities/usereventrole/controllers/UserEventRoleCreateRequest.java
@@ -4,11 +4,7 @@ import com.eventify.api.constants.EventRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.UUID;
