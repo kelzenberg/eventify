cf1e873abdbdd342a7e3308882f048bc9440344b
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-06-14 19:47:40 +0200

    remove unneeded import

--- api/src/main/java/com/eventify/api/GlobalExceptionHandler.java
+++ api/src/main/java/com/eventify/api/GlobalExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.mail.MessagingException;
