5f9ae4274a53ea45ca7dab692e49d719c294ea13
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-06-26 17:14:12 +0200

    add verification attributes to User entity

--- api/src/main/java/com/eventify/api/entities/user/data/User.java
+++ api/src/main/java/com/eventify/api/entities/user/data/User.java
@@ -7,8 +7,12 @@ import com.eventify.api.entities.Views;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.*;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 import javax.persistence.*;
+import java.nio.ByteBuffer;
+import java.util.Date;
+import java.util.UUID;
 
 @Entity
 // Users is reserved keyword. Fix by manually escaping: https://stackoverflow.com/a/50222377
@@ -48,11 +52,38 @@ public class User extends BaseEntity {
     @Transient
     private EventRole eventRole;
 
+    @JsonIgnore
+    @ToString.Exclude
+    @Setter(AccessLevel.PRIVATE)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    private Date verifiedAt;
+
+    @JsonIgnore
+    @ToString.Exclude
+    @Setter(AccessLevel.PRIVATE)
+    @NonNull
+    @Column(updatable = false, nullable = false)
+    private UUID verificationHash = UUID.randomUUID();
+
+    public String getVerificationHash() {
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+        buffer.putLong(this.verificationHash.getMostSignificantBits());
+        buffer.putLong(this.verificationHash.getLeastSignificantBits());
+        return Base64.encodeBase64URLSafeString(buffer.array());
+    }
+
     @Builder
     public User(@NonNull String email, @NonNull String password, @NonNull String displayName) {
         super();
         this.email = email;
         this.password = password;
         this.displayName = displayName;
+    }
+
+    public boolean verificationHashIsValid(String verificationHash) {
+        byte[] bytes = Base64.decodeBase64(verificationHash);
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        return this.verificationHash.equals(new UUID(buffer.getLong(), buffer.getLong()));
     }
 }
