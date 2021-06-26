278c6c4be6302a72d1b5e7428526a6bb06fb02d7
Jannis Rieger <omniskopus@gmail.com>
2021-06-26 21:02:10 +0200

    added comments to stateKeeper

--- app/common/stateKeeper.js
+++ app/common/stateKeeper.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
+// The stateKeeper module holds the state of the user session together with some basic user information that can be accessed anywhere in the app.
+
+/*  The UserContext provides basic information about the current user with the following structure:
+    {
+        "id": "3e8a5ae4-ef25-45f4-be61-31b0c73db495",
+        "email": "mail@address.de",
+        "displayName": "Jane Doe"
+    }
+*/
 export const UserContext = React.createContext(null);
 
+// isAuthenticated returns true if there is a login token stores.
+// It does not check back with the server if this is still valid.
 export function isAuthenticated() {
     return localStorage.getItem("USER_TOKEN") != null;
 }
 
-export function getDisplayName() {
-    return "Username";
-}
-
-export function setCredentials(token, userInfo) {
+// setCredentials stores a new login token
+export function setCredentials(token) {
     localStorage.setItem("USER_TOKEN", token);
 }
 
+// getAuthenticationToken returns the stored login token
 export function getAuthenticationToken() {
     return localStorage.getItem("USER_TOKEN");
 }
@@ -22,9 +31,10 @@ export function getAuthenticationToken() {
 export function getUserInfo() {
     try {
         let data = JSON.parse(localStorage.getItem("USER_INFO"));
-        if (data == null) throw new Error();
+        if (data == null) throw new Error("data is null");
         return data;
     } catch(e) {
+        console.warn("unable to get user info:", err);
         return {
             "id": "",
             "createdAt": "2021-06-02T00:06:45.528+00:00",
@@ -54,10 +64,12 @@ export function maybeUpdateUserInfo(newUserInfo) {
     return false;
 }
 
+// setUserInfo stores the new user information
 function setUserInfo(userInfo) {
     localStorage.setItem("USER_INFO", JSON.stringify(userInfo));
 }
 
+// clearLogin removes any stored information
 export function clearLogin() {
     localStorage.removeItem('USER_TOKEN');
     localStorage.removeItem('USER_INFO');
