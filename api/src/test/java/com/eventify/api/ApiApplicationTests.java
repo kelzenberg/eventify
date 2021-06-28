c22cebd722ea7bcddf24389eae414219e47b6513
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-06-28 20:31:04 +0200

    fix test configuration

--- api/src/test/java/com/eventify/api/ApiApplicationTests.java
+++ api/src/test/java/com/eventify/api/ApiApplicationTests.java
@@ -1,26 +1,24 @@
 package com.eventify.api;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApiApplicationTests {
 
     @Autowired
     Environment env;
-    
-    @Test
+
+    @BeforeEach
     void contextLoads(){
         assertNotNull(env);
     }
