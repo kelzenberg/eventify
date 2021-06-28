a0c1d380e57e1971ffb12e9b6e167fb77a2b8577
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-06-28 20:21:55 +0200

    add jUnit to project with simple test

--- api/src/test/java/com/eventify/api/ApiApplicationTests.java
+++ api/src/test/java/com/eventify/api/ApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.eventify.api;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApiApplicationTests {
 
+    @Autowired
+    Environment env;
+    
     @Test
-    void contextLoads() {
+    void contextLoads(){
+        assertNotNull(env);
+    }
+
+    @Test
+    void activeProfileIsLocal() {
+        List<String> environments = Arrays.asList(env.getActiveProfiles());
+        assertEquals(environments.size(), 1);
+        assertEquals(environments.get(0), "local");
     }
 
 }
